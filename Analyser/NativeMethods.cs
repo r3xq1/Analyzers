33a0db2e9ee8c3a5e2bf1770d6b8dd63ae155321
r3xq1 <79271705+r3xq1@users.noreply.github.com>
2021-10-05 18:12:43 +0300

    Update NativeMethods.cs

--- Analyser/NativeMethods.cs
+++ Analyser/NativeMethods.cs
@@ -5,8 +5,7 @@
     internal static class NativeMethods
     {
         [DllImport("User32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool GetLastInputInfo(ref Structures.LASTINPUTINFO plii);
+        public static extern bool GetLastInputInfo(ref Structures.LASTINPUTINFO plii);
 
         [DllImport("Kernel32.dll")]
         public static extern uint GetLastError();
